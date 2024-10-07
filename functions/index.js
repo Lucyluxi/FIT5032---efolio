c5ccc8a6ccd122240d6f13be8526f0a7679ce45a
Xi Lu <lucyluxi2001@gmail.com>
2024-10-07 11:08:23 +1100

    Week10

---
+++ functions/index.js
@@ -0,0 +1,59 @@
+const { onRequest } = require("firebase-functions/v2/https");
+const admin = require("firebase-admin");
+const cors = require("cors")({ origin: true });
+
+admin.initializeApp();
+
+exports.countBooks = onRequest((req, res) => {
+  cors(req, res, async () => {
+    try {
+      const booksCollection = admin.firestore().collection("books");
+      const snapshot = await booksCollection.get();
+      const count = snapshot.size;
+
+      res.status(200).send({count});
+    } catch (error) {
+      console.error("Error counting books:", error.message);
+      res.status(500).send("Error counting books");
+    }
+  });
+});
+
+exports.allBooks = onRequest((req, res) => {
+  cors(req, res, async () => {
+    try {
+      const booksCollection = admin.firestore().collection("books");
+      const snapshot = await booksCollection.get();
+
+      const books = snapshot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+      }));
+
+      res.status(200).send({books});
+    } catch (error) {
+      console.error("Error fetching books:", error.message);
+      res.status(500).send("Error fetching books");
+    }
+  });
+});
+
+exports.addBook = onRequest((req, res) => {
+    cors(req, res, async () => {
+      try {
+        const { isbn, name } = req.body;
+
+        const newBook = {
+            isbn: isbn,
+            name: name.toUpperCase(),
+        };
+
+        const docRef = await admin.firestore().collection("books").add(newBook);
+
+        res.status(200).send({ message: "Book added successfully", bookId: docRef.id });
+      } catch (error) {
+        console.error("Error counting books:", error.message);
+        res.status(500).send("Error counting books");
+      }
+    });
+  });
---
+++ src/views/GetAllBookAPI.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-4 offset-md-4 text-center mb-5">
+                <pre>{{ jsondata }}</pre>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            jsondata: null,
+            error: null,
+        };
+    },
+    mounted(){
+        this.getAllBookAPI();
+    },
+    methods: {
+        async getAllBookAPI() {
+            try {
+                const response = await axios.get('https://allbooks-r7ejfwewqq-uc.a.run.app');
+                this.jsondata = response.data;
+                this.error = null;
+            } catch (error) {
+                console.error('Error fetching book count: ', error);
+                this.count = null;
+                this.error = 'Error fetching book count';
+            }
+        }
+    }
+}
+</script>
