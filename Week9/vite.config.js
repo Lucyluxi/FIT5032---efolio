29cfda620dc53dd982b0e185ae16c05626bbb9f5
Xi Lu <lucyluxi2001@gmail.com>
2024-10-18 20:55:27 +1100

    Main branch Update

--- Week9/vite.config.js
+++ Week9/vite.config.js
@@ -4,8 +4,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '',
+  build: {
+    outDir: 'dist'
+  },
   plugins: [
     vue(),
     vueDevTools(),
@@ -14,5 +17,8 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/FIT5032---efolio/'
+    : '/'
 })
