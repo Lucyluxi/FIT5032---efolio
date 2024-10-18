5f35438c048aa2daad2ca71d2425fc3504cdb2f5
Xi Lu <lucyluxi2001@gmail.com>
2024-10-18 20:37:14 +1100

    Trigger deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '',
   build: {
@@ -18,5 +17,8 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/FIT5032---efolio/'
+    : '/'
 })
