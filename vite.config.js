265ec7eda25451023561015b5948212181a675db
Xi Lu <lucyluxi2001@gmail.com>
2024-10-18 20:26:29 +1100

    Trigger deployment

--- vite.config.js
+++ vite.config.js
@@ -9,16 +9,3 @@ module.exports = {
     ? '/FIT5032---efolio/'
     : '/'
 }
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
