c032a487d491a07a390c05b6f07053ca39227dad
Xi Lu <lucyluxi2001@gmail.com>
2024-10-18 20:32:01 +1100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,19 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/FIT5032---efolio/'
-    : '/'
-}
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '',
+  build: {
+    outDir: 'dist'
+  },
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
